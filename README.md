159e149c91b9e2e893e40cfcbe157e6cc7dfa9f6
wangfeng <wangjunfeng@bianxianmao.com>
2019-09-26 12:33:24 +0800

    添加HTTP请求模板在js中，同时添加介绍

---
+++ README.md
@@ -0,0 +1,4 @@
+#H5移动适配方案
+    在这个工程中是一个简单是适配方案，你可以在HTML中写入你的标签 css中写入样式 js中写入逻辑代码
+
+    如果你看不懂的话：去我的微博 https://blog.csdn.net/huhudeni/article/details/101198547
--- index.js
+++ index.js
@@ -12,6 +12,37 @@ class index {
         $('#text-b').text('我是来测试js驱动html的')
     }
     bin(){}
+
+
+
+
+
+    sendHttp(){
+
+
+        $.ajax({
+            url: 'url的链接地址',
+            type: type,
+            data: JSON.stringify({ '参数对象' }),
+            dataType: 'json',
+            crossDomain: true, //强制使用5+跨域 --这关掉了 我有时候在ios端发送不了数据 所以我就打开了
+            contentType: 'application/json',
+            beforeSend: function(request) {
+                request.setRequestHeader('你的头部关键字如：H',123);//添加的头部的相关信息 123是传入的参数
+            },
+            success: function (res) {//成功的回调
+                console.log('res:',res); 
+            },
+            error:function (xhr,text) {//失败的回调
+                console.log('text:',text,'xhr:',xhr);
+                if(xhr.status == 200){//这个200码说明返回时正常的--也不是错误
+                  
+                }else {//下面是返回的信息是出现错误的信息
+                    
+                }
+            }
+        });
+    }
 }
       
 new index();
